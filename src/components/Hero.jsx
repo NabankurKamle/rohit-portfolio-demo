81ef199e4d5e950274c473639642498240dd192e
Nabankur <nabankurkamle@gmail.com>
2023-07-26 23:33:07 +0530

    Name Updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,17 +6,17 @@ const Hero = () => {
       <div className="items-center justify-center text-white h-[80vh] overflow-hidden hidden md:flex">
         <div className="flex flex-col justify-center items-center lg:items-end w-full h-full text-[150px]">
           <div className="relative md:-right-5">
-            <span className="block uppercase leading-[180px] font-extrabold tracking-[5px] ">
+            <span className="block relative uppercase leading-[180px] font-extrabold tracking-[5px] ">
               Rohit
+              <img
+                className=" h-[201px] absolute -top-[150px] -right-10"
+                src="/img/profile.svg"
+                alt=""
+              />
             </span>
             <span className="block uppercase leading-[120px] font-extrabold tracking-[5px] [-webkit-text-stroke-color:#01e9de] [-webkit-text-fill-color:transparent] [-webkit-text-stroke-width:1px]">
               Patra
             </span>
-            <img
-              className=" h-[210px] absolute -top-[158px] -right-16"
-              src="/img/profile.svg"
-              alt=""
-            />
           </div>
         </div>
         <div className="h-full w-full hidden lg:block">
