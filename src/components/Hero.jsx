7bcd147d1d132f40d54850e40e14d897ad5c3172
Nabankur <nabankurkamle@gmail.com>
2023-07-26 23:43:44 +0530

    Name Updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,18 +3,18 @@ import Dummy from "./Dummy";
 const Hero = () => {
   return (
     <div className="w-full" id="home">
-      <div className="items-center justify-center text-white h-[80vh] overflow-hidden hidden md:flex">
+      <div className="items-center justify-center text-white h-[60vh] lg:h-[80vh] overflow-hidden hidden md:flex">
         <div className="flex flex-col justify-center items-center lg:items-end w-full h-full text-[150px]">
-          <div className="relative md:-right-5">
-            <span className="block relative uppercase leading-[180px] font-extrabold tracking-[5px] ">
+          <div className="relative md:-right-8">
+            <span className="block relative uppercase leading-[180px] font-extrabold tracking-[6px] ">
               Rohit
               <img
-                className="h-[200px] absolute bottom-[128px] -right-10"
+                className="h-[210px] absolute bottom-[128px] -right-[58px]"
                 src="/img/profile.svg"
                 alt=""
               />
             </span>
-            <span className="block uppercase leading-[120px] font-extrabold tracking-[5px] [-webkit-text-stroke-color:#01e9de] [-webkit-text-fill-color:transparent] [-webkit-text-stroke-width:1px]">
+            <span className="block uppercase leading-[120px] font-extrabold tracking-[6px] [-webkit-text-stroke-color:#01e9de] [-webkit-text-fill-color:transparent] [-webkit-text-stroke-width:1px]">
               Patra
             </span>
           </div>
